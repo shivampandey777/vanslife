8248871555f19aafb6ce36284c553f14517be2eb
shivampandey777 <126339561+shivampandey777@users.noreply.github.com>
2024-02-06 17:37:59 +0530

    uploaded vanslife

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home'
+import About from './About'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+        <header>
+            <Link className="homeLink" to="/">#VANLIFE</Link>
+          <nav>
+            <Link className="aboutLink" to="/About">About</Link>
+            <Link className="vansLink" to="/Vans">Vans</Link>
+          </nav>
+        </header>       
+         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} /> 
+        </Routes>
+        <footer>
+          <h1 className='foot'>Ⓒ2022 #VANLIFE</h1>
+        </footer>
+   </Router>
+
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import background from './homepage.png';
+
+
+export default function Home() {
+    return (
+        <div  className="home" >
+        <h1>You got the travel plans, we got the travel vans.</h1>
+        <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+        <button className="find-btn">Find your van</button>
+        </div>
+    );
+}
