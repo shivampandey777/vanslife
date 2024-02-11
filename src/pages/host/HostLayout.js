f1b77f2c7b6d822607c6d1cb7dc9cfcc957d5e44
shivampandey777 <126339561+shivampandey777@users.noreply.github.com>
2024-02-11 23:12:04 +0530

    navlink change

--- src/pages/host/HostLayout.js
+++ src/pages/host/HostLayout.js
@@ -2,23 +2,27 @@ import React from "react";
 import { NavLink ,Outlet } from "react-router-dom";
 
 export default function HostLayout() {
-
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+}
     return (
         <>
         <nav  className="host-nav">
          <NavLink 
-           className={({ isActive }) => isActive ? "my-nav" : null}
-           end
+          style={({ isActive }) => isActive ? activeStyles : null}
+            end 
            to="/host">
             Dashboard
          </NavLink>
          <NavLink 
-            style={({ isActive }) => isActive ? "my-nav" : null}
+            style={({ isActive }) => isActive ? activeStyles : null}
             to="/host/income">
             Income
          </NavLink>
          <NavLink 
-            style={({ isActive }) => isActive ? "my-nav" : null}
+            style={({ isActive }) => isActive ? activeStyles : null}
             to="/host/reviews">
              Reviews
          </NavLink>
